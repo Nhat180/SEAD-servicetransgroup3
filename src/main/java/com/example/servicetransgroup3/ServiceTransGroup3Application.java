5e8e9a55a4f357f899dafd6a7cbcdc363faf7cbc
Nhat180 <s3740975@rmit.edu.vn>
2021-12-24 11:27:12 +0700

    First Commit

---
+++ src/main/java/com/example/servicetransgroup3/ServiceTransGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.servicetransgroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTransGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTransGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servicetransgroup3/ServiceTransGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicetransgroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTransGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
