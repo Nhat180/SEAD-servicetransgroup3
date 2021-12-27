9fefd36682f4bec3e8100af96bdd25e3af9d9ecf
Nhat180 <s3740975@rmit.edu.vn>
2021-12-28 02:37:35 +0700

    Update ServiceTransGroup3Application.java

--- src/main/java/com/example/servicetransgroup3/ServiceTransGroup3Application.java
+++ src/main/java/com/example/servicetransgroup3/ServiceTransGroup3Application.java
@@ -3,7 +3,10 @@ package com.example.servicetransgroup3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class ServiceTransGroup3Application {
 
 	public static void main(String[] args) {
