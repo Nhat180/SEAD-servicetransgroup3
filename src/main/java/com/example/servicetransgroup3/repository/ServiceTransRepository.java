eeac0951a45bd5ba6fd7f6400c977b13edb0ae32
Nhat180 <s3740975@rmit.edu.vn>
2021-12-28 02:37:39 +0700

    Create ServiceTransRepository.java

---
+++ src/main/java/com/example/servicetransgroup3/repository/ServiceTransRepository.java
@@ -0,0 +1,13 @@
+package com.example.servicetransgroup3.repository;
+
+import com.example.servicetransgroup3.model.ServiceTrans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ServiceTransRepository extends JpaRepository<ServiceTrans, Long> {
+    List<ServiceTrans> findAllByMechanic (String mechanic);
+}
